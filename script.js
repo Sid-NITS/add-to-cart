aa3e9ca5e26c51956dad4b670e4f339826cd0e7a
Siddharth Shankar <128209199+Sid-NITS@users.noreply.github.com>
2023-08-19 11:35:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-7d301-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL:  "copy and paste your firebase database reference url here"
 }
 
 const app = initializeApp(appSettings)
